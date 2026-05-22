a5c504ad4f91abb7533c15ef68f831cff276cf05
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2026-05-22 10:51:25 -0500

    add get-scalers

---
+++ examples/didaq-get-scalers.c
@@ -0,0 +1,19 @@
+#include "didaq.h"
+#include <stdlib.h>
+
+int main (int nargs, char ** args)
+{
+  didaq_setup_t setup = { 
+    .spi_device = nargs > 1 ? args[1] : "/dev/spidev1.0", 
+    .spi_en_gpio_label = "NSPIBUS_EN", 
+    .spi_speed = nargs > 2 ? atoi(args[2]) : 0 
+  };
+
+  didaq_dev_t * dev = didaq_open(&setup);
+
+  didaq_scalers_t scal;
+  didaq_read_scalers(dev, &scal);
+
+  didaq_dump_scalers( &scal, stdout);
+  return didaq_close(dev);
+}
