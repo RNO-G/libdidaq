5f1ce5940613872e2ee2b9f97d1d7e212bf08f9d
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2026-06-24 11:15:24 -0500

    improve macros, get some docs ready

--- src/didaq_regs.h
+++ src/didaq_regs.h
@@ -1,9 +1,12 @@
 #ifndef _DIDAQ_REGS_H
 #define _DIDAQ_REGS_H
 
-/** Low-level access to DIDAQ registers
+/** Low-level access to DIDAQ registers. Most users should use didaq.h
  *
- * Functions are generated from the below X-macro
+ * Unless you enjoy preprocessor soup?
+ *
+ * Functions are generated from the below X-macro.
+ *
  */
 #include <stdint.h>
 #include <unistd.h>
@@ -11,12 +14,25 @@
 
 
 
-/** Completes all scheduled transactions */
+/**
+ * Completes all scheduled transactions  that were scheduled using the sched form of register functions. 
+ * @param dev the didaq device
+ * */
 int didaq_complete(didaq_dev_t * dev);
 
-/** X-Macro to define DIDAQ Registers  and access functions */
+/** X-Macro to define DIDAQ Registers  and access functions
+ *
+ *  name:  name of the register
+ *  baseaddr: base address, only relevant for naddr > 1
+ *  naddr: number of similar adderresses (for when there is an offset)
+ *  rw:  RW register (0 is readonly)
+ *  varlen: if non-zero, may be read multiple times in a row up to varlen times.
+ *          this will prepopulate a buffer
+ *  type:  the type we read to. the sizeof this type is relevant for endianess.
+ *
+ * */
 #define DIDAQ_REGS(REG)\
-      /** name      , addr0 , naddr,   rw, varlen,  type */\
+      /** name      , baseaddr , naddr,   rw, varlen,  type */\
   REG(REVISION      , 0x0000,    1,     0,   0,    uint32_t)\
   REG(BOARD_ID      , 0x0001,    1,     0,   0,    uint32_t)\
   REG(LED_SWITCH    , 0x0002,    1,     1,   0,    uint32_t)\
@@ -52,6 +68,8 @@ int didaq_complete(didaq_dev_t * dev);
 
 
 #define DIDAQ_DEFINE_ENUM(NAME, ADDR, NADDR, RW, VAR, T) DIDAQ_##NAME,
+/** Enum of all didaq registers */
+
 enum didaq_regs
 {
   DIDAQ_REGS(DIDAQ_DEFINE_ENUM)
@@ -145,36 +163,66 @@ typedef struct
 
 
 
-#define DIDAQ_VARREAD_PARAM_1  size_t num_rbytes,
-#define DIDAQ_VARREAD_PARAM_0
-#define DIDAQ_VARREAD_PARAM(X)  DIDAQ_VARREAD_PARAM_##X
+/* Tiny preprocessor detection toolkit to detect special values without enumerating everything. */
+#define DIDAQ_CAT(a, b)         DIDAQ_CAT_(a, b)
+#define DIDAQ_CAT_(a, b)        a ## b
+#define DIDAQ_PROBE(x)          x, 1,                         /* x is always placeholder */
+#define DIDAQ_CHECK(...)        DIDAQ_CHECK_(__VA_ARGS__, 0,) /* 1 if a probe fired, else 0 */
+#define DIDAQ_CHECK_(x, n, ...) n
+#define DIDAQ_IIF(c)            DIDAQ_CAT(DIDAQ_IIF_, c)      /* IIF(1)(t,f)=t  IIF(0)(t,f)=f */
+#define DIDAQ_IIF_1(t, ...)     t
+#define DIDAQ_IIF_0(t, ...)     __VA_ARGS__
 
-#define DIDAQ_NUM_ADDR_PARAM_N size_t offset,
-#define DIDAQ_NUM_ADDR_PARAM_1
-#define DIDAQ_NUM_ADDR_PARAM_5 DIDAQ_NUM_ADDR_PARAM_N
-#define DIDAQ_NUM_ADDR_PARAM_10 DIDAQ_NUM_ADDR_PARAM_N
-#define DIDAQ_NUM_ADDR_PARAM_12 DIDAQ_NUM_ADDR_PARAM_N
-#define DIDAQ_NUM_ADDR_PARAM_24 DIDAQ_NUM_ADDR_PARAM_N
-#define DIDAQ_NUM_ADDR_PARAM(X)  DIDAQ_NUM_ADDR_PARAM_##X
+/* varlen: 0 is special (fixed length -> no num_rbytes arg), anything else gets it */
+#define DIDAQ_IS_ZERO(X)       DIDAQ_CHECK(DIDAQ_CAT(DIDAQ_IS_ZERO_, X))
+#define DIDAQ_IS_ZERO_0        DIDAQ_PROBE(~)
+#define DIDAQ_VARREAD_PARAM(X)  DIDAQ_IIF(DIDAQ_IS_ZERO(X))(/*fixed*/, /*var*/ size_t num_rbytes,)
+#define DIDAQ_DOC_VARREAD_PARAM(X)  DIDAQ_IIF(DIDAQ_IS_ZERO(X))("" , "size_t num_rbytes, ")
+
+/* naddr: 1 is special (single address -> no offset arg), anything else gets it */
+#define DIDAQ_IS_ONE(X)      DIDAQ_CHECK(DIDAQ_CAT(DIDAQ_IS_ONE_, X))
+#define DIDAQ_IS_ONE_1       DIDAQ_PROBE(~)
+#define DIDAQ_NUM_ADDR_PARAM(X) DIDAQ_IIF(DIDAQ_IS_ONE(X))(/*scalar*/, /*multi*/ size_t offset,)
+#define DIDAQ_DOC_NUM_ADDR_PARAM(X) DIDAQ_IIF(DIDAQ_IS_ONE(X))("", "size_t offset, ")
 
 
 
+// generates read functions
 #define DIDAQ_READ_FNS(NAME,ADDR,NADDR,RW,VAR,T) \
   int didaq_read_##NAME(didaq_dev_t * dev, DIDAQ_NUM_ADDR_PARAM(NADDR) DIDAQ_VARREAD_PARAM(VAR) T * val);\
   int didaq_sched_read_##NAME(didaq_dev_t * dev, DIDAQ_NUM_ADDR_PARAM(NADDR) DIDAQ_VARREAD_PARAM(VAR) T * val);
 
+
+// generates API documentation for read functions (just as a string)
+#define DIDAQ_DOC_READ_FNS(NAME,ADDR,NADDR,RW,VAR,T) \
+  "/** Read " #NAME " register, synchronously*/"\
+  "int didaq_read_" #NAME "(didaq_dev_t * dev, " DIDAQ_DOC_NUM_ADDR_PARAM(NADDR) DIDAQ_DOC_VARREAD_PARAM(VAR) " T * val);\n"\
+  " /** Schedule a read of the  " #NAME " register. May happen immediately if spidev buffer is full. */"\
+  "int didaq_sched_read_" #NAME "(didaq_dev_t * dev, " DIDAQ_DOC_NUM_ADDR_PARAM(NADDR) DIDAQ_DOC_VARREAD_PARAM(VAR) " T * val);\n\n";
+
+
+
 DIDAQ_REGS(DIDAQ_READ_FNS)
 
-#define DIDAQ_IF_RW_1(NAME, NADDR, T) \
+//generates write functions for registers not marked read only
+#define DIDAQ_WRITE_FNS(NAME, ADDR, NADDR, RW, VAR, T) \
+    DIDAQ_IIF(RW) (\
     int didaq_write_##NAME(didaq_dev_t * dev, DIDAQ_NUM_ADDR_PARAM(NADDR) const T * val);\
-    int didaq_sched_write_##NAME(didaq_dev_t * dev, DIDAQ_NUM_ADDR_PARAM(NADDR) const T * val);
-#define DIDAQ_IF_RW_0(NAME, NADDR, T)
+    int didaq_sched_write_##NAME(didaq_dev_t * dev, DIDAQ_NUM_ADDR_PARAM(NADDR) const T * val);\
+    , /* no write for you */)
 
-#define DIDAQ_IF_RW(RW, NAME, NADDR, T) DIDAQ_IF_RW_INNER(RW, NAME, NADDR, T)
-#define DIDAQ_IF_RW_INNER(RW, NAME, NADDR,  T) DIDAQ_IF_RW_##RW(NAME, NADDR, T)
-#define DIDAQ_WRITE_FNS(NAME, ADDR, NADDR, RW, VAR,  T) DIDAQ_IF_RW(RW, NAME, NADDR, T)
+//and the API docs
+#define DIDAQ_DOC_WRITE_FNS(NAME, ADDR, NADDR, RW, VAR, T) \
+    DIDAQ_IIF(RW) (\
+    "/** Writes to " #NAME " immediately */\n"\
+    "int didaq_write_" #NAME "(didaq_dev_t * dev, " DIDAQ_DOC_NUM_ADDR_PARAM(NADDR) " const T * val);\n"\
+    "/** Writes to " #NAME " eventually */\n"\
+    "int didaq_sched_write_" #NAME "(didaq_dev_t * dev, " DIDAQ_DOC_NUM_ADDR_PARAM(NADDR) " const T * val);\n\n"\
+    , /* no write for you */)
 
 
 DIDAQ_REGS(DIDAQ_WRITE_FNS)
+
+
 
 #endif
