c61506b3192e0f184f059d809157170d15d84f49
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2026-07-01 14:04:54 -0500

    formatting

--- src/didaq.c
+++ src/didaq.c
@@ -431,8 +431,8 @@ int didaq_dump(didaq_dev_t * dev, FILE * f, int flags)
   ret += fprintf(f, "  phased_ctl = { .en_trig = %u, .en_trig_to_data = %u, .req_consec_wins = %u, .divide_by_2 = %u, .channel_mask = 0b%b, .beam_mask = 0b%b  } \n; " , 
                         dev->phased_ctl.en_trig, dev->phased_ctl.en_trig_to_data, dev->phased_ctl.req_consec_wins, dev->phased_ctl.divide_by_2, dev->phased_ctl.channel_mask, dev->phased_ctl.beam_mask);
   ret += fprintf(f, "  coin_ctl[2] = {\n "
-                    "  { .en_module = %u, .en_readout = %u, .num_coinc = %u, .coinc_win = %u, .include_mask = 0b%b }, \n; "
-                    "  { .en_module = %u, .en_readout = %u, .num_coinc = %u, .coinc_win = %u, .include_mask = 0b%b }\n  }\n; "
+                    "    { .en_module = %u, .en_readout = %u, .num_coinc = %u, .coinc_win = %u, .include_mask = 0b%b }, \n "
+                    "    { .en_module = %u, .en_readout = %u, .num_coinc = %u, .coinc_win = %u, .include_mask = 0b%b }\n  }\n; "
                     , dev->coin_ctl[0].en_module, dev->coin_ctl[0].en_readout, dev->coin_ctl[0].num_coinc, dev->coin_ctl[0].coin_win, dev->coin_ctl[0].include_mask
                     , dev->coin_ctl[1].en_module, dev->coin_ctl[1].en_readout, dev->coin_ctl[1].num_coinc, dev->coin_ctl[1].coin_win, dev->coin_ctl[1].include_mask);
 
@@ -526,6 +526,7 @@ int didaq_set_thresholds( didaq_dev_t * dev,
 {
   if (!dev) return -ENODEV;
   if (!phased && !coin) return 0;
+
   int ret = 0;
 
   if (phased)
